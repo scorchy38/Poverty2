f593b4b70810cf01875bf8a9487756177e0bde63
Shubh Saraswat <VKUMARSARASWAT@GMAIL.COM>
2021-07-14 21:13:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import * as firebase from 'firebase';
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDGFLc72MAf_p2pZLACWboSGsnwXOMDEOQ",
-  authDomain: "portfolio-eac01.firebaseapp.com",
-  databaseURL: "https://portfolio-eac01.firebaseio.com",
-  projectId: "portfolio-eac01",
-  storageBucket: "portfolio-eac01.appspot.com",
-  messagingSenderId: "901854854156",
-  appId: "1:901854854156:web:9b6609a30f6635e4cdc229"
-};
+ var firebaseConfig = {
+    apiKey: "AIzaSyDGFLc72MAf_p2pZLACWboSGsnwXOMDEOQ",
+    authDomain: "portfolio-eac01.firebaseapp.com",
+    databaseURL: "https://portfolio-eac01.firebaseio.com",
+    projectId: "portfolio-eac01",
+    storageBucket: "portfolio-eac01.appspot.com",
+    messagingSenderId: "901854854156",
+    appId: "1:901854854156:web:5a2b7e0181c29a30cdc229"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
